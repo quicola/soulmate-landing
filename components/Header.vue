8d4f24ca2b1937b720e75d1b875362db6ce76684
quicola <ariadneta@gmail.com>
2023-04-11 08:37:26 -0400

    Update Header.vue
    Recuerda que el nombre es Solvmate ;-)

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,7 @@
     <header class="bg-gray-800 sticky top-0 z-10">
         <div class="container mx-auto px-4 h-16 flex items-center justify-between">
         <div>
-            <h1>Soulmate</h1>
+            <h1>Solvmate</h1>
         </div>
         <nav>
             <ul class="hidden md:flex items-center space-x-4">
