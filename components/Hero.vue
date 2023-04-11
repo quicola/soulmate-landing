371e637b03fbc536691d1eb9bc399b747ace6143
quicola <ariadneta@gmail.com>
2023-04-11 08:41:58 -0400

    Update Hero.vue
    reworded

--- components/Hero.vue
+++ components/Hero.vue
@@ -2,8 +2,8 @@
   <section class="hero">
     <div class="container mx-auto px-4">
       <div class="content">
-        <h1 class="text-white text-4xl font-bold mb-8">Using AI to connect you with others, regardless of language.</h1>
-        <h2>Where language is no longer a barrier to connection, our AI-powered matchmaking and communication platform allows you to effortlessly connect with others from all over the world, breaking down language obstacles and making true communication possible.</h2>
+        <h1 class="text-white text-4xl font-bold mb-8">Using AI to connect you to people around the world, in your language of choice.</h1>
+        <h2>Our AI-powered matchmaking and communication platform allows you to effortlessly communicate with others from all over the world, breaking down language barriers and helping you learn and practice new languages.</h2>
         <button class="bg-blue-500 mt-8 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
           Join the waiting list >
         </button>
